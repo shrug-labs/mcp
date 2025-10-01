50063e45840cf333a581212a807ccf7eac3b7e3b
Richard Gebhardt <richard.gebhardt@oracle.com>
2025-10-01 15:58:53 -0400

    chore: add copyright headers (#67)

--- src/oci-network-load-balancer-mcp-server/oracle/oci_network_load_balancer_mcp_server/server.py
+++ src/oci-network-load-balancer-mcp-server/oracle/oci_network_load_balancer_mcp_server/server.py
@@ -1,3 +1,9 @@
+"""
+Copyright (c) 2025, Oracle and/or its affiliates.
+Licensed under the Universal Permissive License v1.0 as shown at
+https://oss.oracle.com/licenses/upl.
+"""
+
 import os
 from logging import Logger
 from typing import Annotated
