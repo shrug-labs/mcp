35b5defa66ba58e4a002df163f32e68e2b022406
Richard Gebhardt <richard.gebhardt@oracle.com>
2025-10-09 14:18:08 -0400

    chore: combine README and .gitignore
    Signed-off-by: Richard Gebhardt <richard.gebhardt@oracle.com>

--- src/datetime-helper-mcp-server/oracle/datetime_helper_mcp_server/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Copyright (c) 2025, Oracle and/or its affiliates.
-Licensed under the Universal Permissive License v1.0 as shown at
-https://oss.oracle.com/licenses/upl.
-"""
-
-__project__ = "oracle.datetime-helper-mcp-server"
-__version__ = "0.1.0"
